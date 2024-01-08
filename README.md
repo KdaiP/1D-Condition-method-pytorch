8926e5b777c9c7abc2b8b5de698423865d4f1317
KdaiP <bingpohun@outlook.com>
2024-01-08 14:00:12 +0800

    add FiLM and CLM

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# 1D-Condition-method-pytorch
-Enhance your neural networks with advanced conditioning methods.
+<div align="center">
+
+# 1D-Condition-Method-PyTorch
+
+_✨ Enhance your neural networks with advanced conditioning methods. ✨_
+
+</div>
+
+## Introduction 🚀
+
+This repository presents PyTorch implementations of various methods to inject additional information, such as time embeddings in diffusion UNet or speaker embeddings in speech synthesis, into your models. Enhance your network's performance and capabilities with these advanced conditioning techniques.
+
+## Features 🌟
+
+- **FiLM Layer**: Incorporate the [FiLM: Visual Reasoning with a General Conditioning Layer](https://arxiv.org/abs/1709.07871) into your models to dynamically influence their behavior based on external information.
+- **Conditional Layer Norm**: Utilize the Conditional Layer Norm strategy from [AdaSpeech](https://arxiv.org/abs/2103.00993) for adaptive and context-aware normalization.
+
+
+## Usage 📘
+
+### FiLM Layer
+
+Incorporate external information into your model's intermediate representations dynamically.
+
+```python
+import torch
+from layers import FiLMLayer
+
+x = torch.randn((16,37,256)) # [batch_size, time, in_channels]
+c = torch.randn((16,1,320)) # [batch_size, 1, cond_channels]
+
+model = FiLMLayer(256, 320)
+output = model(x, c) # [batch_size, time, in_channels]
+```
+
+### Conditional Layer Norm
+
+Adaptively normalize your data based on additional conditioning information.
+
+```python
+import torch
+from layers import ConditionalLayerNorm
+
+x = torch.randn((16,37,256)) # [batch_size, time, in_channels]
+c = torch.randn((16,1,320)) # [batch_size, 1, cond_channels]
+
+model = ConditionalLayerNorm(256, 320)
+output = model(x, c) # [batch_size, time, in_channels]
+```
---
+++ layers.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+class FiLMLayer(nn.Module):
+    def __init__(self, in_channels, cond_channels):
+        """
+        Feature-wise Linear Modulation (FiLM) layer
+        
+        Parameters:
+        in_channels: The number of channels in the input feature maps.
+        cond_channels: The number of channels in the conditioning input.
+        """
+        super(FiLMLayer, self).__init__()
+        self.in_channels = in_channels
+        self.film = nn.Linear(cond_channels, in_channels * 2)
+
+    def forward(self, x, c):
+        """
+        Parameters:
+        x (Tensor): The input feature maps with shape [batch_size, time, in_channels].
+        c (Tensor): The conditioning input with shape [batch_size, 1, cond_channels].
+        
+        Returns:
+        Tensor: The modulated feature maps with the same shape as input x.
+        """
+        film_params = self.film(c)
+        gamma, beta = torch.chunk(film_params, chunks=2, dim=-1)
+        
+        return gamma * x + beta
+    
+class ConditionalLayerNorm(nn.Module):
+    def __init__(self, in_channels, cond_channels, eps=1e-5):
+        """
+        Conditional Layer Normalization module.
+        
+        Parameters:
+        in_channels: The number of channels in the input feature maps.
+        cond_channels: The number of channels in the conditioning input.
+        eps: A small number to prevent division by zero in normalization.
+        """
+        super(ConditionalLayerNorm, self).__init__()
+        self.eps = eps
+        self.in_channels = in_channels
+        self.cond_channels = cond_channels
+        
+        self.weight_transform = nn.Linear(cond_channels, in_channels)
+        self.bias_transform = nn.Linear(cond_channels, in_channels)
+        
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.constant_(self.weight_transform.weight, 0.0)
+        torch.nn.init.constant_(self.weight_transform.bias, 1.0)
+        torch.nn.init.constant_(self.bias_transform.weight, 0.0)
+        torch.nn.init.constant_(self.bias_transform.bias, 0.0)
+
+    def forward(self, x, c):
+        """
+        Parameters:
+        x (Tensor): The input feature maps with shape [batch_size, time, in_channels].
+        c (Tensor): The conditioning input with shape [batch_size, 1, cond_channels].
+        
+        Returns:
+        Tensor: The modulated feature maps with the same shape as input x.
+        """
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True, unbiased=False)
+        
+        x_normalized = (x - mean) / (std + self.eps)
+        
+        gamma = self.weight_transform(c)
+        beta = self.bias_transform(c)
+        
+        out = gamma * x_normalized + beta
+        
+        return out
